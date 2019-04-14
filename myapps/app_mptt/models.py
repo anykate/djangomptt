6bde7bfbea14c6e79f6bcf5d588376a872dce316
Aniket Aryamane <aryamane.aniket@gmail.com>
2019-04-14 18:03:42 +0530

    Added models.py, templates, views.py, admin.py - Aniket Aryamane

--- myapps/app_mptt/models.py
+++ myapps/app_mptt/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
 
 # Create your models here.
+class Category(MPTTModel):
+    name = models.CharField(max_length=50, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE,
+                            null=True, blank=True, related_name='children')
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name + ' ' + f'<{self.id}>'
--- myapps/app_mptt/views.py
+++ myapps/app_mptt/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Category
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'app_mptt/home.html', {})
+    my_queryset = Category.objects.all()
+    categories=Category.objects.get_queryset_descendants(my_queryset, include_self=True)
+    return render(request, 'app_mptt/home.html', {'categories': categories})
